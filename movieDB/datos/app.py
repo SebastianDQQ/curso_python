094bdd95c3b09ab2401f6e137220c9acfd7bee04
Sebastian Duarte Quijada <a223205136@unison.mx>
2025-03-26 10:55:21 -0700

    Se agrego index, peliculas, base y actores

---
+++ movieDB/datos/app.py
@@ -0,0 +1,24 @@
+'''programa principla de movieDB'''
+from flask import flask,Request,url_for,render_template,redirect
+import os
+import random
+import movie_classes as mc
+ 
+app = flask(__name__)
+sistema = mc.SistemaCine()
+archivo_actores = "datos/movies_db - actores.csv"
+archivo_peliculas = "datos/movies_db - peliculas.csv"
+archivo_relaciones = "datos/movies_db - relacion.csv"
+archivo_usuarios = "datos/movies_db - users_hashed.csv"
+sistema.cargar_csv(archivo_actores, mc.Actor)
+sistema.cargar_csv(archivo_peliculas, mc.Pelicula)
+sistema.cargar_csv(archivo_relaciones, mc.Relacion)
+sistema.cargar_csv(archivo_usuarios, mc.User)
+ 
+@app.route('/')
+def index():
+    '''pagina principal de la aplicacion'''
+    return render_template('index.html')
+ 
+if __name__ == '__main__':
+    app.run(debug=True)
